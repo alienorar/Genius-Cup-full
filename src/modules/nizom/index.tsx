f3a4b80adeb703ed3aa0b9a285d45cc164b3eb20
alienorar <rayhonakhayrullaeva@gmail.com>
2025-04-02 16:12:38 +0500

    savollar data fixed

--- src/modules/nizom/index.tsx
+++ src/modules/nizom/index.tsx
@@ -5,39 +5,39 @@ const sections = [
     id: "1",
     title: "I bob",
     content: (
-      <>
-        <h2 className="text-[20px] font-semibold">
+      <div className="flex flex-col gap-2">
+        <h2 className="text-[22px] font-semibold">
           1. Umumiy qoidalar
         </h2>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">1.1.</strong>	Mazkur Nizom Osiyo Texnologiyalar universitetining innovatsion tashabbusi sifatida respublikamizning barcha umumiy o‘rta ta’lim maktab o‘quvchilari, akademik litsey va kasbiy ta’lim muassasalari talabalari orasidan iqtidorli, bilimdon va yaratuvchan yoshlarga e’tibor qaratgan holda, ularni oliy ta’limga keng jalb etish hamda kelajakdagi muvaffaqiyatga yo‘l ochish maqsadida tashkil etayotgan “Genius-Cup” onlayn IQ musobaqasining tashkil etish tartibini belgilaydi.
         </p>
         <p className="text-[16px] text-[#01284099] poppin"><strong className="text-black">1.2.</strong> Imtihonlarda O‘zbekiston Respublikasidagi barcha umumiy o‘rta ta’lim maktablarining 3-11 sinf o‘quvchilari hamda akademik litsey va kasbiy ta’lim muassasalarining 1-2 bosqich talabalari ishtirok etishi mumkin.</p>
-      </>
+      </div>
     ),
   },
   {
     id: "2",
     title: "II bob",
     content: (
-      <>
-        <h2 className="text-[20px] font-semibold">
+      <div className="flex flex-col gap-2">
+        <h2 className="text-[22px] font-semibold">
           2.	Musobaqaning asosiy maqsad va vazifalari
         </h2>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">2.1</strong>  Musobaqaning asosiy maqsadi yoshlarimizning bilimini, mantiqiy fikrlashini va tezkor qaror qabul qilish qobiliyatini aniqlash va rivojlantirishdan iborat bo‘lib, quyidagi maqsadlarni ham qamrab oladi:
-          <li>	Yoshlarimizga zamonaviy bilim va texnologiyalarni o‘zlashtirish imkoniyatini yaratish;</li>
-          <li>	Ularning intellektual imkoniyatlarini oshirish va oliy ta’limga qiziqishini rag‘batlantirish.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">	Yoshlarimizga zamonaviy bilim va texnologiyalarni o‘zlashtirish imkoniyatini yaratish;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">	Ularning intellektual imkoniyatlarini oshirish va oliy ta’limga qiziqishini rag‘batlantirish.</li>
 
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">2.2</strong> Musobaqaning ustuvor Vazifasi qatnashuvchilarning shaxsiy rivojlanishini qo‘llab-quvvatlash bilan birga, jamiyatimizning intellektual salohiyatini oshirishga ham xizmat qiladi:
-          <li>Yoshlarimizga zamonaviy bilim va texnologiyalarni o‘zlashtirish imkoniyatini yaratish;</li>
-          <li>Innovatsion fikrlash va kreativ yondoshuvni rag‘batlantirish;</li>
-          <li>Oliy ta’lim tizimiga kirish uchun malaka va qobiliyatlarni rivojlantirish;</li>
-          <li>	Qatnashchilarni sog‘lom raqobat muhitida sinovdan o‘tkazish orqali ularning imkoniyatlarini aniq baholash.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Yoshlarimizga zamonaviy bilim va texnologiyalarni o‘zlashtirish imkoniyatini yaratish;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Innovatsion fikrlash va kreativ yondoshuvni rag‘batlantirish;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Oliy ta’lim tizimiga kirish uchun malaka va qobiliyatlarni rivojlantirish;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">	Qatnashchilarni sog‘lom raqobat muhitida sinovdan o‘tkazish orqali ularning imkoniyatlarini aniq baholash.</li>
         </p>
-      </>
+      </div>
     ),
   },
   {
@@ -45,13 +45,13 @@ const sections = [
     title: "III bob",
     content: (
       <>
-        <h2 className="text-[20px] font-semibold">
+        <h2 className="text-[22px] font-semibold">
           3. Ishtirok etish shartlari
         </h2>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">3.1</strong> Musobaqada qatnashish uchun mo‘ljallangan auditoriyani quyidagilardan iborat:
-          <li>O‘zbekiston Respublikasidagi barcha umumiy o‘rta ta’lim maktablarining 3–11-sinf o‘quvchilari;</li>
-          <li>Akademik litsey va kasbiy ta’lim muassasalarining 1–2 bosqich talabalari.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">O‘zbekiston Respublikasidagi barcha umumiy o‘rta ta’lim maktablarining 3–11-sinf o‘quvchilari;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Akademik litsey va kasbiy ta’lim muassasalarining 1–2 bosqich talabalari.</li>
 
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
@@ -65,7 +65,7 @@ const sections = [
     title: "IV bob",
     content: (
       <>
-        <h2 className="text-[20px] font-semibold">4. Musobaqani tashkil etish tartibi</h2>
+        <h2 className="text-[22px] font-semibold">4. Musobaqani tashkil etish tartibi</h2>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.1</strong> Musobaqaning birinchi bosqichi ishtirokchilarning umumiy bilim, 
           mantiqiy fikrlash va tezkor qaror qabul qilish qobiliyatini sinovdan o‘tkazish uchun 
@@ -75,38 +75,38 @@ const sections = [
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.2</strong> Musobaqa qatnashuvchilarining sinf kesimidan kelib chiqib, mos guruhlar uchun test savollari 
           shakllantiriladi:
-          <li>Birinchi guruh - 3–5-sinf o‘quvchilari;</li>
-          <li>Ikkinchi guruh - 6–8-sinf o‘quvchilari;</li>
-          <li>Uchinchi guruh - 9–11-sinf o‘quvchilari va akademik litsey, kasbiy ta’lim muassasalarining 1–2 bosqich talabalari.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Birinchi guruh - 3–5-sinf o‘quvchilari;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Ikkinchi guruh - 6–8-sinf o‘quvchilari;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Uchinchi guruh - 9–11-sinf o‘quvchilari va akademik litsey, kasbiy ta’lim muassasalarining 1–2 bosqich talabalari.</li>
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.3</strong> Test savollari va baholash tizimi:
-          <li>Har bir ishtirokchiga 20 ta savol taqdim etiladi;</li>
-          <li>To‘g‘ri javob uchun 5 ball, noto‘g‘ri javob uchun 0 ball beriladi;</li>
-          <li>Eng yuqori ball 100 ballni tashkil etadi.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Har bir ishtirokchiga 20 ta savol taqdim etiladi;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">To‘g‘ri javob uchun 5 ball, noto‘g‘ri javob uchun 0 ball beriladi;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Eng yuqori ball 100 ballni tashkil etadi.</li>
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.4</strong> Testni bajarish uchun 60 daqiqa vaqt ajratiladi. 
           Eng yuqori ball to‘plagan 100 nafar ishtirokchi g‘olib deb tanlanadi.
-          <li>3–5-sinf: 20 nafar;</li>
-          <li>6–8-sinf: 40 nafar;</li>
-          <li>9–11-sinf va yuqorilar: 40 nafar.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">3–5-sinf: 20 nafar;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">6–8-sinf: 40 nafar;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">9–11-sinf va yuqorilar: 40 nafar.</li>
           Ball teng bo‘lsa, testni tugatish vaqti hal qiluvchi omil hisoblanadi.
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.5</strong> G‘oliblarga mukofotlar:
-          <li>Har bir bosqich g‘olibiga sertifikat va 100 000 so‘m miqdorida pul mukofoti;</li>
-          <li>Ikkinchi bosqichga yo‘llanma.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Har bir bosqich g‘olibiga sertifikat va 100 000 so‘m miqdorida pul mukofoti;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Ikkinchi bosqichga yo‘llanma.</li>
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.6</strong> Ikkinchi bosqich 2025-yil 10-may kuni Osiyo Texnologiyalar universitetida o'tkaziladi.
         </p>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">4.7</strong> Final bosqichida har bir guruhdan eng yuqori natija qayd etgan 3 nafar g‘olib quyidagi mukofotlarga ega bo‘ladi:
-          <li>1-o‘rin: 3 000 000 so‘m;</li>
-          <li>2-o‘rin: 2 000 000 so‘m;</li>
-          <li>3-o‘rin: 1 000 000 so‘m.</li>
-          <li>Uchinchi guruhning eng yuqori ball to‘plagan 10 nafari Osiyo Texnologiyalar universitetida bir yil bepul o‘qish imkoniyatiga ega bo‘ladi.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">1-o‘rin: 3 000 000 so‘m;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">2-o‘rin: 2 000 000 so‘m;</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">3-o‘rin: 1 000 000 so‘m.</li>
+          <li className="text-[16px] text-[#01284099] py-1 poppin">Uchinchi guruhning eng yuqori ball to‘plagan 10 nafari Osiyo Texnologiyalar universitetida bir yil bepul o‘qish imkoniyatiga ega bo‘ladi.</li>
         </p>
       </>
     ),
@@ -116,7 +116,7 @@ const sections = [
     title: "V bob",
     content: (
       <>
-        <h2 className="text-[20px] font-semibold">IV. Yakuniy qoidalar</h2>
+        <h2 className="text-[22px] font-semibold">5. Yakuniy qoidalar</h2>
         <p className="text-[16px] text-[#01284099] poppin">
           <strong className="text-black">7.</strong> Ishtirokchilar tomonidan taqdim etilgan materiallar bo‘yicha izoh yoki sharh berilmaydi va ular egalariga qaytarilmaydi.
         </p>
--- src/modules/sovrin/index.tsx
+++ src/modules/sovrin/index.tsx
@@ -3,53 +3,54 @@ import Gold from "../../assets/gold.svg"
 import Silver from "../../assets/silver.svg"
 import Bronze from "../../assets/bronze.svg"
 import { motion } from "framer-motion";
-const PrizeFundSection = () => {
 
+const PrizeFundSection = () => {
   const prizes = [
     {
       id: 1,
       image: Gold,
-      amount: "6.800.000",
-      times: "20",
+      amount: "3.000.000",
+      description: "1-o‘rin uchun mukofot",
       color: "text-[#1E9FD9]",
     },
     {
       id: 2,
       image: Silver,
-      amount: "5.440.000",
-      times: "16",
+      amount: "2.000.000",
+      description: "2-o‘rin uchun mukofot",
       color: "text-[#1E9FD9]",
     },
     {
       id: 3,
       image: Bronze,
-      amount: "4.080.000",
-      times: "12",
+      amount: "1.000.000",
+      description: "3-o‘rin uchun mukofot",
       color: "text-[#1E9FD9]",
     },
   ];
 
   return (
     <>
-      <section className="flex flex-col lg:flex-row items-center justify-between gap-8  custom-container poppin">
+      <section className="flex flex-col lg:flex-row items-center justify-between gap-8 custom-container poppin">
         {/* Left Side (Text) */}
-        <motion.div initial={{ opacity: 0, y: 50 }}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className=" lg:px-[100px] lg:py-[100px] px-[20px] text-center flex flex-col justify-center items-center lg:items-start  ">
-          <h2 className="text-[#1E9FD9]  text-center  lg:text-start text-[34px] md:text-3xl font-bold">
-            450 million so‘m
+          className="lg:px-[100px] lg:py-[100px] px-[20px] text-center flex flex-col justify-center items-center lg:items-start"
+        >
+         <h2 className="text-[#1E9FD9] text-center lg:text-start text-[34px] md:text-3xl font-bold">
+            Ajoyib imkoniyatlar
           </h2>
           <h3 className="text-[#000000] text-center lg:text-start text-[28px] md:text-2xl font-bold mt-1">
-            Umumiy sovrin jamg‘armasi
+            Genius-Cup musobaqasi g'oliblari uchun
           </h3>
-          <p className="text-[#01284099] mt-4 text-[18px] poppin md:text-base text-center lg:text-start leading-relaxed lg:w-[543px] ">
-            Olimpiadaning uchinchi bosqichida oltin, kumush, bronza medallarini
-            olgan 10–11-sinf o‘quvchilariga Xalqaro Nordik universitetida 100 foizlik
-            grant asosida kirish huquqini beruvchi sertifikatlar (uch yil muddatga
-            amal qiluvchi) taqdim etiladi va uning alohida elektron reestri yuritiladi.
+          <p className="text-[#01284099] mt-4 text-[18px] poppin md:text-base text-center lg:text-start leading-relaxed lg:w-[543px]">
+            Musobaqada eng yaxshi natijalarni ko'rsatgan ishtirokchilar nafaqat pul mukofotlari, 
+            balki Osiyo Texnologiyalar Universitetida tahsil olish imkoniyatini qo'lga kiritadilar. 
+            9-11-sinf o'quvchilari uchun alohida grantlar ajratilgan.
           </p>
-          <button className="relative mt-3.5 h-10 w-40 overflow-hidden border-[2px] border-[#1E9FD9] text-[16px] rounded-[30px] text-[#1E9FD9] font-[500] poppin  transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-[#1E9FD9] before:duration-300 before:ease-out hover:text-white hover:shadow-[#1E9FD9] hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
+          <button className="relative mt-3.5 h-10 w-40 overflow-hidden border-[2px] border-[#1E9FD9] text-[16px] rounded-[30px] text-[#1E9FD9] font-[500] poppin transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-[#1E9FD9] before:duration-300 before:ease-out hover:text-white hover:shadow-[#1E9FD9] hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
             <a href="https://t.me/Genius_cup_bot" target="_blank" rel="noopener noreferrer" className="relative z-10">
               Ro'yxatdan O'tish
             </a>
@@ -57,10 +58,12 @@ const PrizeFundSection = () => {
         </motion.div>
 
         {/* Right Side (Image) */}
-        <motion.div initial={{ opacity: 0, y: 50 }}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="w-full max-w-sm lg:max-w-md">
+          className="w-full max-w-sm lg:max-w-md"
+        >
           <img
             src={PrizeImg}
             alt="Winners Celebration"
@@ -68,36 +71,39 @@ const PrizeFundSection = () => {
           />
         </motion.div>
       </section>
-      <section className=" custom-container ">
-        <div className="flex flex-col md:flex-row justify-center gap-3  py-10 text-center lg:px-[60px]">
+
+      <section className="custom-container">
+        <div className="flex flex-col md:flex-row justify-center gap-3 py-10 text-center lg:px-[60px]">
           {prizes.map((prize) => (
-            <div key={prize.id} className="flex   flex-col w-full items-center">
-              <img src={prize.image} alt={`Rank ${prize.id}`} className="w-24 md:w-28 lg:w-32 transition-transform ease-linear duration-500 hover:transform hover:rotate-y-360" />
+            <div key={prize.id} className="flex flex-col w-full items-center">
+              <img
+                src={prize.image}
+                alt={`Rank ${prize.id}`}
+                className="w-24 md:w-28 lg:w-32 transition-transform ease-linear duration-500 hover:transform hover:rotate-y-360"
+              />
               <p className="text-gray-700 mt-4 text-lg md:text-xl">
-                Bazaviy hisoblash miqdorining <br />
-                <span className="font-bold">{prize.times} baravari</span>{" "}
-                <span className={`font-bold ${prize.color}`}>{prize.amount}</span> so‘m
+                {prize.description}: <br />
+                <span className={`font-bold ${prize.color}`}>{prize.amount}</span> so'm
               </p>
-            
             </div>
           ))}
         </div>
-
       </section>
+
       <section className="py-8 text-black custom-container px-20 md:px-10 lg:px-20">
-        <h2 className="text-xl md:text-2xl font-bold mb-4 text-center lg:text-start">Uchinchi bosqich g‘oliblari</h2>
-        <p className=" lg:text-[20px] text-[18px]  font-semibold md:text-base mb-4 text-center lg:text-start">
-          Olimpiadaning uchinchi bosqichida oltin, kumush, bronza medallarini olgan 10–11-sinf o‘quvchilarga
-          Xalqaro Nordik universitetida 100 foizlik grant asosida kirish huquqini beruvchi sertifikatlar
-          (uch yil muddatga amal qiluvchi) taqdim etiladi va uning alohida elektron reestri yuritiladi.
+        <h2 className="text-xl md:text-2xl font-bold mb-4 text-center lg:text-start">Final bosqich g'oliblari</h2>
+        <p className="lg:text-[20px] text-[18px] font-semibold md:text-base mb-4 text-center lg:text-start">
+          Musobaqaning final bosqichida umumiy guruhlar kesimida har bir guruhdan eng kam vaqt sarflagan holda,
+          eng yuqori natijaga qayd etgan 3 nafar g'olib saralanadi.
         </p>
-        <p className=" lg:text-[20px] text-[18px]  font-semibold md:text-base mb-2 text-center lg:text-start">
-          Olimpiadaning uchinchi bosqichida g‘olib bo‘lgan birinchi guruh o‘quvchilari (3-9-sinflar kesimida)
-          va ularning rahbarlariga quyidagi miqdorda pul mukofotlari beriladi:
+        <p className="lg:text-[20px] text-[18px] font-semibold md:text-base mb-2 text-center lg:text-start">
+          Birinchi o'rinni qayd etgan ishtirokchilar <span className="text-[#1E9FD9]">3.000.000</span>  so'm,
+          ikkinchi o'rinni qayd etgan ishtirokchi <span className="text-[#1E9FD9]">2.000.000</span> so'm,
+          uchinchi o'rinni qayd etgan ishtirokchi <span className="text-[#1E9FD9]">1.000.000</span> so'm pul mukofotlari bilan taqdirlanadi.
         </p>
-        <p className=" lg:text-[20px] text-[18px]  md:text-base font-semibold text-center lg:text-start">
-          Ikkinchi guruh g‘oliblariga (10–11-sinf) jami 15 ming AQSh dollari
-          (so‘m ekvivalentida 187 500 000 so‘m) miqdorida mukofot jamg‘armasi ajratiladi.
+        <p className="lg:text-[20px] text-[18px] md:text-base font-semibold text-center lg:text-start">
+          Uchinchi guruh (9-11-sinflar) ishtirokchilarining eng yuqori ball to'plagan 10 nafari Osiyo
+          Texnologiyalar universitetida bir yil bepul o'qish imkoniyatini qo'lga kiritadi.
         </p>
       </section>
     </>
