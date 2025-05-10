a22129447d0b21721a5ca00d8d3ecf87bc7c3ba9
alienorar <rayhonakhayrullaeva@gmail.com>
2025-05-10 16:40:20 +0500

    faqs added

--- src/modules/savollar/index.tsx
+++ src/modules/savollar/index.tsx
@@ -12,22 +12,35 @@ const faqData = {
     },
     {
       savol: "Ro‘yxatdan o‘tish qanday amalga oshiriladi?",
-      javob: "Telegramdagi <a type='outlined' href='https://t.me/Genius_cup_bot' className='text-[#1E9FD9]' target='_blank'>@Genius_cup_bot</a> yoki <a href='https://t.me/Genius_cup2_bot' target='_blank'>@Genius_cup2_bot</a> orqali 2025-yil 1-maygacha ro‘yxatdan o‘tish kerak."
+      javob: "Telegramdagi <a type='outlined' href='https://t.me/Genius_cup_bot' className='text-[#1E9FD9]' target='_blank'>@Genius_cup_bot</a> yoki <a href='https://t.me/Genius_cup2_bot' target='_blank'>@Genius_cup2_bot</a> orqali ro‘yxatdan o‘tish kerak."
     },
+    
+    {
+      savol: "Test boshlanish sanasi avvaldan qanday tarzda xabar beriladi?",
+      javob: "Osiyo texnologiyalar universiteti va musobaqa  rasmiy telegram sahifasida hamda musobaqa telegram botlarida e'lon qilinadi."
+    },
+    {
+      savol: "Testning birinchi (onlayn) bosqichi qachon va qayerda bo‘lib o‘tadi (web-manzil yoki platforma nomi)",
+      javob: "20-may kuni <a type='outlined' href='https://t.me/Genius_cup_bot' className='text-[#1E9FD9]' target='_blank'>@Genius_cup_bot</a> yoki <a href='https://t.me/Genius_cup2_bot' target='_blank'>@Genius_cup2_bot</a> orqali bo'lib o'tadi. "
+    },
+    {
+      savol: "Test savollari bilan tanishish uchun namunalar berolasizmi?",
+      javob: "Namunalar musobaqa rasmiy telegram sahifasida uzluksiz tarzda berib boriladi. "
+    },
+    {
+      savol: "2-bosqich test musobaqasi o‘tkaziladigan binoga ota-onalardan tashqari uztozlarimizni kelishiga ham ruxsat etiladimi? ",
+      javob: "Albatta, ruxsat etiladi ! "
+    }
+  ],
+  qoidalar: [
     {
       savol: "Musobaqa necha bosqichdan iborat?",
       javob: "2 bosqich: onlayn test (10-may) va universitet binosida final."
     },
-    {
+     {
       savol: "Test qanday baholanadi?",
       javob: "Har bir to‘g‘ri javob 5 ball, maksimum 100 ball. Bir xil ballda testni tezroq tugatgan ishtirokchi ustunlik qiladi."
     },
-    {
-      savol: "Birinchi bosqichda qancha vaqt beriladi?",
-      javob: "20 ta savolga 60 daqiqa vaqt."
-    }
-  ],
-  qoidalar: [
     {
       savol: "Agar ikki ishtirokchi bir xil ball to‘plasa, kim g‘olib bo‘ladi?",
       javob: "Testni tezroq tugatgan ishtirokchi g‘alaba qozonadi."
@@ -37,8 +50,8 @@ const faqData = {
       javob: "Nizomga zid harakatlar (masalan, botdan noqonuniy foydalanish) diskvalifikatsiyaga olib keladi."
     },
     {
-      savol: "Ishtirokchilar o‘z javoblarini qayta ko‘rib chiqishlari mumkinmi?",
-      javob: "Yo‘q, test yakunlangach javoblarni tahrirlash imkoni yo‘q."
+      savol: "Navbatdagi bosqichga o‘tganimni qanday bilaman? ",
+      javob: "<a type='outlined' href='https://t.me/Genius_cup_bot' className='text-[#1E9FD9]' target='_blank'>@Genius_cup_bot</a> yoki <a href='https://t.me/Genius_cup2_bot' target='_blank'>@Genius_cup2_bot</a> orqali bilib olsihingiz mumkin."
     }
   ],
   mukofotlar: [
@@ -47,14 +60,18 @@ const faqData = {
       javob: "Sertifikat + 100 000 so‘m pul mukofoti va finalga yo‘llanma."
     },
     {
-      savol: "Finalda g‘oliblarga nimalar beriladi?",
+      savol: "Finalda g‘oliblari nimalar bilan taqdirlanadi?",
       javob: "1-o‘rin: 3 000 000 so‘m, 2-o‘rin: 2 000 000 so‘m, 3-o‘rin: 1 000 000 so‘m. 9–11-sinf top-10 talabasi universitetda 1 yil bepul o‘qish huquqini qo‘lga kiritadi."
+    },
+    {
+      savol: "Eng yuqori natija bilan g‘olib bo‘lgan ishtirokchi, sovrindan tashqari yana qanday imtiyozlarga ega bo‘ladi? ",
+      javob: "Ishtirokchilarining eng yuqori ball to‘plagan 10 nafari bir yil imtiyozli tarzda Osiyo  Texnologiyalar universitetida bir yil bepul o‘qish imkoniyatini qo‘lga kiritishadi"
     },
     {
       savol: "Mukofot pullari qanday topshiriladi?",
       javob: "Bank orqali yoki universitet tomonidan belgilangan usulda (plastik karta raqamiga o‘tkazma)."
     }
-  ]
+  ],
 };
 const FAQSection = () => {
   const { category } = useParams<{ category: keyof typeof faqData }>();
@@ -95,9 +112,9 @@ const FAQSection = () => {
                 className="flex items-center justify-between  w-full text-left font-medium text-lg py-2"
                 onClick={() => toggleFAQ(index)}
               ><div className="flex gap-4 md:gap-5 justify-center items-center">
-                  <span  >
-                    <img src={ChatImg} alt="chat img "  />
-                  </span>
+                  <div className="h-8 w-8"  >
+                    <img src={ChatImg} alt="chat img " className="object-cover" />
+                  </div>
                   <span>{item.savol}</span>
                 </div>
                 <span className={`transform transition-transform ${openIndex === index ? "rotate-180" : ""}`}><FiChevronDown /></span>
